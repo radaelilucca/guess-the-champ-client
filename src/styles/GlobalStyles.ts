9ed6b13d5f135b1c58a5d17048ab7e06872e3dea
Lucca Radaeli <radaelidev@gmail.com>
2022-08-15 15:04:09 -0300

    fix(styles): fix some globalStyles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,13 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 .app-wrapper {
-  background-color: ${(props) => props.theme.colors.darkBackground};
+  
 
 
-  height: 100vh;
-  width: 100vw;
-
+  height: 100%;
+  width: 100%;
   display: flex;
+
+  flex: 1;
 
   justify-content: center;
   align-items:center;
@@ -16,7 +17,35 @@ export const GlobalStyles = createGlobalStyle`
   font-family: ${(props) => props.theme.fontFamily.poppins}; 
   
   * {
-    transition: all 150ms ease;
+      transition: all 150ms ease;
     }
+  }
+
+  *, 
+  *:after,
+  *:before {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      text-decoration: none;
+  }
+
+  body {
+      font-size: 100%;
+      list-style-type: none;
+      
+      height: 100%;
+      width: 100%;
+      flex: 1;
+
+      background-color: ${(props) => props.theme.colors.darkBackground};
+
+  }
+
+  #root {
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
   }
 `;
